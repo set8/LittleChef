1bf622914d03715eceb48f12cd8004ac86ea62cb
Aditya-Baradi <Adityabaradi@gmail.com>
2024-02-10 22:53:26 -0500

    SET COMMIT

--- mongodb.py
+++ mongodb.py
@@ -94,3 +94,28 @@ def deleteUser(usr):
         if info == usr:
             value = diction.get(i)
             collection.delete_one({usr:value})
+def setAllergy(usr, allergy):
+    diction = collection.find_one()
+    for i in diction:
+        info = i
+        if info == usr:
+            value = diction.get(i)
+            for i in allergy:
+                value["allergy"].append(i)
+            dict_vals= value.values()
+            final_val= list(dict_vals)
+            deleteUser(usr)
+            createUser(usr, final_val[0],final_val[1],final_val[2],final_val[3],final_val[4])
+
+def setPantry(usr, food, quant):
+    diction = collection.find_one()
+    for i in diction:
+        info = i
+        if info == usr:
+            value = diction.get(i)
+            value["pantry_"][food] = quant
+            dict_vals= value.values()
+            final_val= list(dict_vals)
+            deleteUser(usr)
+            createUser(usr, final_val[0],final_val[1],final_val[2],final_val[3],final_val[4])
+
